113bc60d681beb8041ed0b5d0830a612db83fd7c
Peggycyggep <peggycyggep@yahoo.com>
2020-12-05 20:50:29 -0800

    revision
    fix the printf message
    add a walking robot

--- script/hello.cpp
+++ script/hello.cpp
@@ -1,5 +1,3 @@
-
-
 #include <gazebo/gazebo.hh>
 
 namespace gazebo
@@ -7,15 +5,13 @@ namespace gazebo
   class Project1Robot : public WorldPlugin
   {
     public: Project1Robot() : WorldPlugin()
-    {
-      printf("Welcome to Project 1 Robot's World!");
-    }
+            {
+              printf("Welcome to Project 1 Robot's World!\n");
+            }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-    {
-      printf("Loading Project 1 Robot's World!");
-    }
+            {
+            }
   };
   GZ_REGISTER_WORLD_PLUGIN(Project1Robot)
 }
-
